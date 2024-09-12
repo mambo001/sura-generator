16d19f8fc84c73fd03683e158b8a544c82d3f2df
Reuben <reuben.gorda@gmail.com>
2024-09-12 14:33:57 +0800

    add array randomizer util

--- apps/web/src/app/utils/index.ts
+++ apps/web/src/app/utils/index.ts
@@ -8,3 +8,13 @@ export function generateRandomColor() {
   const hex = Math.floor(Math.random() * 0xffffff);
   return '#' + hex.toString(16);
 }
+
+export function randomizeArray<T>(array: T[]) {
+  return array
+    .reduce((acc, cur) => {
+      const randomIndex = Math.floor(Math.random() * 1000);
+      acc[randomIndex] = cur;
+      return acc;
+    }, [] as T[])
+    .filter((e) => e);
+}
