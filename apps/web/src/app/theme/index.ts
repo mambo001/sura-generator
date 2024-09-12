67287fec184b818a4d0b17153a18442dd1f41bc9
Reuben <reuben.gorda@gmail.com>
2024-09-12 18:31:16 +0800

    add material ui theming

---
+++ apps/web/src/app/theme/index.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF885B',
+    },
+    secondary: {
+      main: '#FFE5CF',
+    },
+    background: {
+      default: '#557C56',
+    },
+    text: {
+      primary: '#33372C',
+    },
+  },
+});
