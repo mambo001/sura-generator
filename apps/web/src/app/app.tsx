c78f3a8a716082b04e6ec6e281e2390f30413376
Reuben <reuben.gorda@gmail.com>
2024-09-12 18:32:02 +0800

    replace wheel. add confetti. add random text renderer

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Box,
   Button,
@@ -6,104 +6,156 @@ import {
   Container,
   Skeleton,
   Stack,
+  ThemeProvider,
   Typography,
+  useTheme,
 } from '@mui/material';
-import { ISegments } from 'spin-wheel-game';
+import type { WheelDataType } from 'react-custom-roulette';
 
-import { suraList as localSuraList } from './data';
 import { SpinWheel } from './components';
-import { generateRandomColor, generateSura } from './utils';
+import { generateRandomColor } from './utils';
+import ReactConfetti from 'react-confetti';
+import { useWindowSize } from 'react-use';
 
 const API_URL =
   'https://script.google.com/macros/s/AKfycbye-vQ7rkXjBPc5I8yhQo9HQcwBnBDtIC9Wch6mortUj78RwINFQbdY2dJkQAiH9aoIkA/exec';
 
 type Sura = {
   id: number;
-  suraList: string;
+  name: string;
 };
 
-export function App() {
-  const [selectedSura, setSelectedSura] = useState<string | null>(null);
-  const [suraList, setSuraList] = useState<ISegments[] | null>(null);
-  const spinWheelRef = useRef(null);
-  const handleGenerateSuraClick = () => {
-    // const sura = generateSura(suraList);
-    // setSelectedSura(sura);
-  };
+function RandomText(props: { options: string[]; interval: number }) {
+  const [newName, setNewName] = useState('');
 
-  const callback = useCallback(() => {
+  const selectRandomText = useCallback(() => {
+    const index = Math.floor(Math.random() * props.options.length);
+    setNewName(props.options[index]);
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(selectRandomText, props.interval / 100);
+    return () => clearInterval(intervalId);
+  }, [selectRandomText, props.interval]);
+
+  return <Typography variant="h4">{newName}</Typography>;
+}
+
+export function App() {
+  const { width, height } = useWindowSize();
+  const [suraList, setSuraList] = useState<WheelDataType[]>();
+  const [mustStartSpinning, setMustStartSpinning] = useState(false);
+  const [selectedSura, setSelectedSura] = useState<number | null>(null);
+  const theme = useTheme();
+
+  useEffect(() => {
     fetch(API_URL)
       .then((res) => res.json())
-      .then(([res]) => {
+      .then((res) => {
         const { data }: { data: Sura[] } = res;
-        const suraList = data.map((sura) => ({
-          segmentText: sura.suraList,
-          segColor: generateRandomColor(),
-        }));
+        console.log({ data });
+        const suraList: WheelDataType[] = data.map((sura) => {
+          const backgroundColor = generateRandomColor();
+          const textColor = theme.palette.getContrastText(backgroundColor);
+          return {
+            option: sura.name,
+            style: {
+              textColor,
+              backgroundColor,
+            },
+          };
+        });
         setSuraList(suraList);
       });
   }, []);
 
-  useEffect(() => {
-    callback();
-  }, [callback]);
-
-  useEffect(() => {
-    if (!spinWheelRef.current) return;
-    const spinWheel: HTMLDivElement = spinWheelRef.current;
-    const wheel = spinWheel.querySelector('#wheel');
-    const canvas = spinWheel.querySelector('#canvas');
-  }, [suraList]);
-
   function handleSpinClick() {
-    if (!spinWheelRef.current) return;
-    const spinWheel: HTMLDivElement = spinWheelRef.current;
-    const canvas = spinWheel.querySelector('#canvas') as HTMLCanvasElement;
-    if (!canvas) return;
-    canvas.click();
+    setMustStartSpinning(true);
   }
 
   return (
-    <Container>
-      <Card
-        style={{
-          marginTop: '15vh',
-        }}
-      >
-        {suraList && suraList.length > 0 ? (
-          <Stack gap={2} padding={2} alignItems={'center'}>
+    <ThemeProvider theme={theme}>
+      <ReactConfetti
+        recycle={true}
+        run={Boolean(!mustStartSpinning && selectedSura)}
+        width={width}
+        height={height}
+        numberOfPieces={1000}
+        hidden={mustStartSpinning}
+      />
+      <Container>
+        <Card
+          style={{
+            marginTop: '15vh',
+          }}
+        >
+          {suraList && suraList.length > 0 ? (
             <Stack gap={2} padding={2} alignItems={'center'}>
-              <Box ref={spinWheelRef}>
-                <SpinWheel options={suraList} />
-              </Box>
+              <Stack gap={2} padding={2} alignItems={'center'}>
+                <SpinWheel
+                  options={suraList}
+                  mustStartSpinning={mustStartSpinning}
+                  spinDuration={0.5}
+                  prizeNumber={Math.floor(
+                    Math.random() * Number(suraList.length)
+                  )}
+                  onStopSpinning={(number) => {
+                    setSelectedSura(number);
+                    setMustStartSpinning(false);
+                  }}
+                />
+                {mustStartSpinning ? (
+                  // <Skeleton
+                  //   sx={{
+                  //     fontSize: 64,
+                  //   }}
+                  //   width={300}
+                  //   variant="text"
+                  // />
+                  <RandomText
+                    interval={10_000}
+                    options={suraList.map((e) => String(e.option))}
+                  />
+                ) : (
+                  // <Typography variant="h4">
+                  //   {suraList[randomOptionIndex].option}
+                  // </Typography>
+                  selectedSura && (
+                    <Typography variant="h4">
+                      {suraList[selectedSura].option}
+                    </Typography>
+                  )
+                )}
+              </Stack>
+              <Button
+                disabled={!suraList || suraList.length < 1}
+                fullWidth
+                variant="contained"
+                onClick={handleSpinClick}
+              >
+                Spin the wheel
+              </Button>
             </Stack>
-            <Button
-              disabled={!suraList || suraList.length < 1}
-              fullWidth
-              onClick={handleSpinClick}
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                flex: 1,
+                // height: 300,
+                justifyContent: 'center',
+                alignItems: 'center',
+                padding: 2,
+                gap: 4,
+                flexDirection: 'column',
+              }}
             >
-              Spin the wheel
-            </Button>
-          </Stack>
-        ) : (
-          <Box
-            sx={{
-              display: 'flex',
-              flex: 1,
-              // height: 300,
-              justifyContent: 'center',
-              alignItems: 'center',
-              padding: 2,
-              gap: 4,
-              flexDirection: 'column',
-            }}
-          >
-            <Skeleton variant="circular" width={300} height={300} />
-            <Skeleton variant="rectangular" width={300} height={40} />
-          </Box>
-        )}
-      </Card>
-    </Container>
+              <Skeleton variant="circular" width={300} height={300} />
+              <Skeleton variant="rectangular" width={300} height={40} />
+            </Box>
+          )}
+        </Card>
+      </Container>{' '}
+    </ThemeProvider>
   );
 }
 
--- apps/web/src/app/components/wheel.tsx
+++ apps/web/src/app/components/wheel.tsx
@@ -1,34 +1,78 @@
-import {
-  SpinWheel as SWGSpinWheel,
-  ISpinWheelProps,
-  ISegments,
-} from 'spin-wheel-game';
+import { useEffect, useState } from 'react';
+import { Wheel } from 'react-custom-roulette';
+import type { WheelDataType } from 'react-custom-roulette';
 
 export interface SpinWheelProps {
-  options: ISegments[];
+  options: WheelDataType[];
+  onSpinClick?: () => void;
+  onStopSpinning: (prizeNumber: number) => void;
+  mustStartSpinning: boolean;
+  prizeNumber?: number;
+  spinDuration: number;
+  backgroundColors?: string[];
+  textColors?: string[];
+  outerBorderColor?: string;
+  outerBorderWidth?: number;
+  innerBorderColor?: string;
+  innerBorderWidth?: number;
+  innerRadius?: number;
+  radiusLineColor?: string;
+  radiusLineWidth?: number;
+  fontFamily?: string;
+  fontSize?: number;
+  textDistance?: number;
 }
 
+const backgroundColors = ['#ff8f43', '#70bbe0', '#0b3351', '#f9dd50'];
+const textColors = ['#0b3351'];
+const outerBorderColor = '#eeeeee';
+const outerBorderWidth = 3;
+const innerBorderColor = '#30261a';
+const innerBorderWidth = 0;
+const innerRadius = 1;
+const radiusLineColor = '#eeeeee';
+const radiusLineWidth = 2;
+const fontFamily = 'Roboto';
+const fontSize = 15;
+const textDistance = 55;
+const spinDuration = 1.0;
+const defaultPrizeNumber = 0;
+
+const generateRandomPrizeNumber = (options: WheelDataType[]) => {
+  return Math.floor(Math.random() * Number(options.length));
+};
+
 export function SpinWheel(props: SpinWheelProps) {
-  const handleSpinFinish = (result: string) => {
-    console.log(`Spun to: ${result}`);
-    // Handle the result as needed
-  };
+  const [prizeNumber, setPrizeNumber] = useState(defaultPrizeNumber);
+  useEffect(() => {
+    if (!props.prizeNumber) {
+      const randomPrizeNumber = generateRandomPrizeNumber(props.options);
+      setPrizeNumber(randomPrizeNumber);
+      return;
+    }
 
-  const spinWheelProps: ISpinWheelProps = {
-    segments: props.options,
-    onFinished: handleSpinFinish,
-    primaryColor: 'black',
-    contrastColor: 'white',
-    buttonText: 'Spin',
-    isOnlyOnce: false,
-    size: 150,
-    upDuration: 100,
-    downDuration: 600,
-    fontFamily: 'Arial',
-    arrowLocation: 'top',
-    showTextOnSpin: true,
-    isSpinSound: true,
-  };
-
-  return <SWGSpinWheel {...spinWheelProps} />;
+    setPrizeNumber(props.prizeNumber);
+  }, [props.prizeNumber, props.options]);
+  return (
+    <Wheel
+      mustStartSpinning={props.mustStartSpinning}
+      prizeNumber={prizeNumber}
+      data={props.options}
+      backgroundColors={backgroundColors}
+      textColors={textColors}
+      fontFamily={fontFamily}
+      fontSize={fontSize}
+      outerBorderColor={outerBorderColor}
+      outerBorderWidth={outerBorderWidth}
+      innerRadius={innerRadius}
+      innerBorderColor={innerBorderColor}
+      innerBorderWidth={innerBorderWidth}
+      radiusLineColor={radiusLineColor}
+      radiusLineWidth={radiusLineWidth}
+      spinDuration={spinDuration}
+      startingOptionIndex={2}
+      textDistance={textDistance}
+      onStopSpinning={() => props.onStopSpinning(prizeNumber)}
+    />
+  );
 }
